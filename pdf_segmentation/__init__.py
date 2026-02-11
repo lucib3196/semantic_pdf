521f1f15f6e3b51ce50c512074695d33f4c9061d
Luciano Bermudez <lucianobmecheng69@gmail.com>
2026-02-10 18:26:27 -0800

    Updated project layout and fixed imports

--- pdf_segmentation/__init__.py
+++
@@ -1,9 +0,0 @@
-from graph.graph import (
-    graph as PDFSegmentation,
-    Section,
-    ListOutput,
-    State as SegmentationInput,
-)
-from pdf_image_converter import PDFImageConverter
-from pdf_seperator import PDFSeperator
-from pdf_llm import PDFMultiModalLLM
---
+++ src/pdf_segmentation/__init__.py
@@ -0,0 +1,9 @@
+from pdf_segmentation.graph.graph import (
+    graph as PDFSegmentation,
+    Section,
+    ListOutput,
+    State as SegmentationInput,
+)
+from pdf_segmentation.pdf_image_converter import PDFImageConverter
+from pdf_segmentation.pdf_seperator import PDFSeperator
+from pdf_segmentation.pdf_llm import PDFMultiModalLLM
